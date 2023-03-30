f910509992197d26eb739893eeb8a426a3221230
Cp-John <e0376984@u.nus.edu>
2023-03-30 11:22:50 +0800

    Update intro

--- src/commons/utils/IntroductionHelper.ts
+++ src/commons/utils/IntroductionHelper.ts
@@ -3,14 +3,15 @@ import { Chapter, Variant } from 'c-slang/dist/types';
 import { Links } from './Constants';
 
 const MAIN_INTRODUCTION = `
-This is a C language interpreter, enjoy programming! 
+This is a C language interpreter, which supports Syntax Check, Type Check, Memory Management, Heap Visualization (_printHeap_), Pointer Arithmetic, etc. Enjoy!\n
+NOT SUPPORTED: preprocessor directives, struct, function pointers, array declaration (use _malloc_ instead)
 `;
 
 const HOTKEYS_INTRODUCTION = `
-Currently NOT SUPPORTED: preprocessor directives, struct, function pointers, arrays (use pointers instead).
-
-In the editor on the left, you can use the [_Ace keyboard shortcuts_](${Links.aceHotkeys}) 
-and also the [_Source Academy keyboard shortcuts_](${Links.sourceHotkeys}).
+Tips:
+  1. Call _printEnv()_ to view what is accessable in the current environment
+  2. The editor on the left supports [_Ace keyboard shortcuts_](${Links.aceHotkeys}) 
+    and [_Source Academy keyboard shortcuts_](${Links.sourceHotkeys}).
 
 Developer: Cao Peng, Contact: e0376984@u.nus.edu
 `;
